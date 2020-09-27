a2fda9c9d42337307021db7748466ea86866e33f
Marina <marina.ssaito@gmail.com>
2020-09-27 12:38:31 -0300

    Parte G - Criação da classe DigitalHouseManager

---
+++ src/br/digital/com/DigitalHouseManager.kt
@@ -0,0 +1,8 @@
+package br.digital.com
+
+class DigitalHouseManager(
+        var listaAlunos: MutableList<Aluno>,
+        var listaProfessores: MutableList<Professor>,
+        var listaCursos: MutableList<Curso>,
+        var listaMatriculas: MutableList<Matricula>) {
+}
